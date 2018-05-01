0d3fdf5a4f772c35014122d18b66be6ddd8a6200
Thomas Dinh <thomasdinh@Thomass-MBP.localdomain>
2018-05-01 18:47:02 -0500

    try1

---
+++ interopolation.py
@@ -0,0 +1,5 @@
+#take first and last name of user seperately
+First = input("Please enter your first name.")
+Last = input("Please enter your last name.")
+
+print ("Hello My name is " + First + "," Last)
